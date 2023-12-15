d855e3ef5ef2621013b653f0e5dd72bff6ea9d1b
sgupta <suryanigupta10@gmail.com>
2023-12-14 18:28:26 -0800

    BinaryPrinter IP

--- src/_03_Printing_Binary/_01_BinaryPrinter.java
+++ src/_03_Printing_Binary/_01_BinaryPrinter.java
@@ -10,7 +10,9 @@ public class _01_BinaryPrinter {
 
     public void printByteBinary(byte b) {
         // We first want to print the bit in the one's place
-    	System.out.println(b[0]);
+    	String bytestr = Integer.toBinaryString(b);
+    	System.out.println(bytestr.charAt(0));
+    	
         // Shift b seven bits to the right
     	int bee = b >> 7;
         // Use the & operator to "mask" the bit in the one's place
@@ -19,6 +21,18 @@ public class _01_BinaryPrinter {
         // Print the result using System.out.print (NOT System.out.println)
     	System.out.print(bee);
         //Use this method to print the remaining 7 bits of b
+    	bee = b >> 6;
+    	System.out.print(bee);
+    	bee = b >> 5;
+    	System.out.print(bee);
+    	bee = b >> 4;
+    	System.out.print(bee);
+    	bee = b >> 3;
+    	System.out.print(bee);
+    	bee = b >> 2;
+    	System.out.print(bee);
+    	bee = b >> 1;
+    	System.out.print(bee);
     }
 
     public void printShortBinary(short s) {
@@ -28,21 +42,27 @@ public class _01_BinaryPrinter {
         // Use bit shifting and masking (&) to save the first
         // 8 bits of s in one byte, and the second 8 bits of
         // s in the other byte
-
+    	first = (byte) (s >> 8);
+    	second = (byte) (s+8 >> 8);
         // Call printByteBinary twice using the two bytes
         // Make sure they are in the correct order
+    	printByteBinary(first);
+    	printByteBinary(second);
     }
 
     public void printIntBinary(int i) {
         // Create 2 short variables
-    	short one;
-    	short two;
+    	short first;
+    	short second;
         // Use bit shifting and masking (&) to save the first
         // 16 bits of i in one short, and the second 16 bits of
         // i in the other short
-
+    	first = (byte) (i >> 16);
+    	second = (byte) (i+16 >> 16);
         // Call printShortBinary twice using the two short variables
         // Make sure they are in the correct order
+    	printByteBinary(first);
+    	printByteBinary(second);
     }
 
     public void printLongBinary(long l) {
